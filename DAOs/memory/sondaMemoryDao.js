cca3a0a275b1f51137cbaf7eaf64539ee3e4c0f9
mlarotonda <mauricio.larotonda@fravega.com.ar>
2024-07-01 20:20:40 -0300

    fix

--- DAOs/memory/sondaMemoryDao.js
+++ DAOs/memory/sondaMemoryDao.js
@@ -21,24 +21,6 @@ class SondaMemoryDao {
     return newSonda;
   }
 
-  updateSonda = async (data) => {
-    const sondaId = parseInt(data.id);
-    const sondaIndex = this.sondas.findIndex((sonda) => sonda.id === sondaId);
-    if (sondaIndex !== -1) {
-      const sonda = this.sondas[sondaIndex];
-      sonda.temperatura = data.temperatura || sonda.temperatura;
-      return 1;
-    }
-    return 0;
-  }
-
-  deleteSonda = async (id) => {
-    const sondaId = parseInt(id);
-    const initialLength = this.sondas.length;
-    this.sondas = this.sondas.filter((sonda) => sonda.id !== sondaId);
-    return initialLength - this.sondas.length;
-  }
-
 }
 
 export default SondaMemoryDao;
